fc8a196077e028cc8785053fa017f74f9907c9fb
Jiangcheng Bao <jbao605@gmail.com>
2020-11-08 23:52:00 -0500

    lc1130 min cost tree from leaf values

---
+++ lc1130_minimum_cost_tree_from_leaf_values.py
@@ -0,0 +1,108 @@
+"""
+1130. Minimum Cost Tree From Leaf Values
+Medium
+
+1585
+
+123
+
+Add to List
+
+Share
+Given an array arr of positive integers, consider all binary trees such that:
+
+Each node has either 0 or 2 children;
+The values of arr correspond to the values of each leaf in an in-order traversal of the tree.  (Recall that a node is a leaf if and only if it has 0 children.)
+The value of each non-leaf node is equal to the product of the largest leaf value in its left and right subtree respectively.
+Among all possible binary trees considered, return the smallest possible sum of the values of each non-leaf node.  It is guaranteed this sum fits into a 32-bit integer.
+
+
+
+Example 1:
+
+Input: arr = [6,2,4]
+Output: 32
+Explanation:
+There are two possible trees.  The first has non-leaf node sum 36, and the second has non-leaf node sum 32.
+
+    24            24
+   /  \          /  \
+  12   4        6    8
+ /  \               / \
+6    2             2   4
+
+
+Constraints:
+
+2 <= arr.length <= 40
+1 <= arr[i] <= 15
+It is guaranteed that the answer fits into a 32-bit signed integer (ie. it is less than 2^31).
+
+"""
+import math
+from functools import lru_cache
+from typing import List
+
+"""
+idea
+
+We are asked to build an optimal binary tree, with all its leaves in-order traverse represented by array arr, but we don't know which leaf node belong to left subtree and which leaf node goes to right subtree.
+Since the given leaf nodes are in-order traverse, there will be a pivot point where all left side of it goes to left subtree, all right of it goes to right subtree. For each subtree, if we know the minimum sum, we can use it to build the parent tree.
+So the original problem can be broken down into subproblems via the pivot point. Then we have the following transition function, where res(i, j) repreents minimum non-leaf nodes sum with leaf nodes represented by arr[i,j]
+
+for k = i ... j:
+    res(i,j) = min(res(i,k) + res(k+1,j) + max(arr[i:k])*max(arr[k+1:j]))
+
+"""
+
+"""
+DP topdown with caching
+
+time O(N^3) - cache stack with two indicies, l, r, plus within the helper function, we have k loop from l to r
+space O(N^2) - cache stack with two indcies, l, r
+"""
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+
+        @lru_cache(None)
+        def helper(l, r):
+            nonlocal arr
+            if l >= r:
+                return 0
+            res = math.inf
+            for k in range(l, r):
+                res = min(res, helper(l, k) + helper(k + 1, r) + max(arr[l:k + 1]) * max(arr[k + 1:r + 1]))
+
+            return res
+
+        return helper(0, len(arr) - 1)
+
+"""
+DP bottom up
+
+time O(N^3)
+space O(N^2)
+"""
+class Solution1:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        n = len(arr)
+        dp = [[math.inf for _ in range(n)] for _ in range(n)]
+        for i in range(n):
+            dp[i][i] = 0
+
+        for l in range(2, n+1): # length from 2 to n
+            for i in range(n+1-l): # starting index i, from 0 to maximum allowed by length l
+                j = i+l-1 # ending index j
+                dp[i][j] = math.inf
+                for k in range(i, j):
+                    dp[i][j] = min(dp[i][j], dp[i][k]+dp[k+1][j]+max(arr[i:k+1])*max(arr[k+1:j+1]))
+
+        return dp[0][n-1]
+
+
+def main():
+    sol = Solution1()
+    assert sol.mctFromLeafValues([6,2,4]) == 32, 'fails'
+
+if __name__ == '__main__':
+   main()
