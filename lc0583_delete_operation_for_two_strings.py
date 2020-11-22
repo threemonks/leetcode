de50243b99e22cd05fc4d17b0d7d735a12f5bcee
Jiangcheng Bao <jbao605@gmail.com>
2020-11-21 21:50:00 -0500

    lc0583 delete operation for two strings

--- lc0583_delete_operation_for_two_strings.py
+++ lc0583_delete_operation_for_two_strings.py
@@ -27,11 +27,51 @@ dp two string
 
 dp[i][j] := min steps required to make two string equal up to word1[:i] and word2[:j]
 
-top down / recursive
+bottom up
+time O(m*n)
+space O(m*n)
 """
+import numpy
 
 
 class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        l1 = len(word1)
+        l2 = len(word2)
+        word1 = '#' + word1
+        word2 = '#' + word2
+        dp = [[0 for _ in range(l2 + 1)] for _ in range(l1 + 1)]
+        dp[0][0] = 0
+
+        for i in range(1, l1 + 1):
+            dp[i][0] = dp[i - 1][0] + 1
+
+        for j in range(1, l2 + 1):
+            dp[0][j] = dp[0][j - 1] + 1
+
+        for i in range(1, l1 + 1):
+            for j in range(1, l2 + 1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1)
+                # print(numpy.matrix(dp))
+
+        return dp[-1][-1]
+
+
+"""
+dp two string
+
+dp[i][j] := min steps required to make two string equal up to word1[:i] and word2[:j]
+
+top down / recursive
+time O(m*n)
+space O(m*n
+"""
+
+
+class Solution1:
     def minDistance(self, word1: str, word2: str) -> int:
 
         @lru_cache(None)
