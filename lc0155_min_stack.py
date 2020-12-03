74fb2979e08866ec09227adc61ae6a973bb84a06
Jiangcheng Bao <jbao605@gmail.com>
2020-12-03 17:59:45 -0500

    lc0155 min stack

---
+++ lc0155_min_stack.py
@@ -0,0 +1,87 @@
+"""
+155. Min Stack
+Easy
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+
+
+Example 1:
+
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+
+Output
+[null,null,null,null,-3,null,0,-2]
+
+Explanation
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin(); // return -3
+minStack.pop();
+minStack.top();    // return 0
+minStack.getMin(); // return -2
+
+
+Constraints:
+
+Methods pop, top and getMin operations will always be called on non-empty stacks.
+
+"""
+import math
+
+"""
+intuition
+push element to stack along with minimum value got so far (associate this min with current value), it can be derived using min for previous value (stack top) and current value
+pop element from stack will remove the min associated with it
+"""
+
+
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x: int) -> None:
+        self.stack.append((x, min(x, self.stack[-1][1] if self.stack else math.inf)))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+def main():
+
+    obj = MinStack()
+    obj.push(-2)
+    obj.push(0)
+    obj.push(-3)
+    assert obj.getMin() == -3, 'fails'
+    obj.pop()
+    assert obj.top() == 0, 'fails'
+    assert obj.getMin() == -2, 'fails'
+
+if __name__ == '__main__':
+   main()
