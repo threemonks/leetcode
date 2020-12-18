f8efe4bdbb38e70f5360d9d2cccd18bf924fb11f
Jiangcheng Bao <jbao605@gmail.com>
2020-12-17 22:43:25 -0500

    lc0713 subarray product less than k - add notes about using sliding window

--- lc0713_subarray_product_less_than_k.py
+++ lc0713_subarray_product_less_than_k.py
@@ -59,6 +59,17 @@ class Solution1:
 """
 use a sliding window that is minimum length with prod (or sums on log of nums[i]) >= k
 https://github.com/wisdompeak/LeetCode/tree/master/Two_Pointers/713.Subarray-Product-Less-Than-K
+
+本题有很明显的滑窗的特征，所以基本思路是用双指针。保证一个乘积小于k的最大窗口，这个窗口内可以构成subarray的数目就是j-i+1;
+
+本题需要注意的一点是，当nums[i]>k时，右指针动不了，而左指针依然会顺移，所以可能会出现j<i的情况，此时只需要重置这两个指针即可：
+
+if (j<i)
+{
+    j = i;
+    product = 1;
+}
+
 """
 
 
