fc430c98665fff32247f358794df7e3a9cd0f36c
Jiangcheng Bao <jbao605@gmail.com>
2020-12-21 12:35:51 -0500

    lc1658 minimum operations to reduce x to zero - add another method of using presum and hashmap lookup given presum value's index, to speed up inner loop

--- lc1658_minimum_operations_to_reduce_x_to_zero.py
+++ lc1658_minimum_operations_to_reduce_x_to_zero.py
@@ -32,6 +32,7 @@ Constraints:
 
 """
 import collections
+import math
 from functools import lru_cache
 from typing import List
 
@@ -43,7 +44,7 @@ use two pointers, iterate (fix) right pointer, explore left pointer to keep a wi
 
 """
 
-class Solution:
+class Solution0:
     def minOperations(self, nums: List[int], x: int) -> int:
         total = sum(nums)
         K = total - x  # find minOperations to reduce x to zero, is equivalent to finding longest subarray with sum K
@@ -66,6 +67,46 @@ class Solution:
         # print('res=%s' % res)
         return n - res if res >= 0 else res
 
+
+"""
+let i be left index, and j be right index of the numbers we picked to reduce x to zero,
+then we have presum[i] + sufsum[j] = x
+we can use two pointers, iterate right index j, for given index j, and explore which left index i has presum[i]=x-sufsum[j]
+we can pre-compute presum and store it in hashmap to make the lookup of i be O(1)
+"""
+
+
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+        n = len(nums)
+
+        presum = 0
+        presum_map = {0: -1}  # no element needed to have presum 0
+        for i in range(n):
+            presum += nums[i]
+            presum_map[presum] = i
+
+        # print('presum_map=%s' % str(presum_map))
+        res = math.inf
+
+        if x in presum_map:
+            res = presum_map[x] + 1
+
+        sufsum = 0
+        for j in range(n - 1, -1, -1):
+            sufsum += nums[j]
+            # print('j=%s sufsum=%s' % (j, sufsum))
+            pre = x - sufsum
+            if pre in presum_map:
+                i = presum_map[pre]
+                if i < j:
+                    res = min(res, (n - j) + i + 1)
+                # print('i=%s j=%s res=%s' % (i, j, res))
+
+        if res == math.inf:
+            res = -1
+
+        return res
 def main():
     sol = Solution()
     assert sol.minOperations([1,1,4,2,3], 5) == 2, 'fails'
@@ -73,6 +114,8 @@ def main():
     assert sol.minOperations([5,6,7,8,9], 4) == -1, 'fails'
 
     assert sol.minOperations([3,2,20,1,1,3], 10) == 5, 'fails'
+
+    assert sol.minOperations([8828,9581,49,9818,9974,9869,9991,10000,10000,10000,9999,9993,9904,8819,1231,6309], 134365) == 16, 'fails'
 
 if __name__ == '__main__':
    main()
