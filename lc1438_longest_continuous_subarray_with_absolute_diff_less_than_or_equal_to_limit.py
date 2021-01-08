8a45f39da0219aa8286a7c89f50b2ea89806c6e9
Jiangcheng Bao <jbao605@gmail.com>
2021-01-07 20:44:33 -0500

    lc1438 longest continuous subarray with absolute difference less than or equal to limit (two pointers with sliding window max and min using deque)

--- lc1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
+++ lc1438_longest_continuous_subarray_with_absolute_diff_less_than_or_equal_to_limit.py
@@ -2,13 +2,6 @@
 1438. Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
 Medium
 
-1075
-
-50
-
-Add to List
-
-Share
 Given an array of integers nums and an integer limit, return the size of the longest non-empty subarray such that the absolute difference between any two elements of this subarray is less than or equal to limit.
 
 
