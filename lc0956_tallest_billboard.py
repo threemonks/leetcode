4a5eeded28c07d1b2bdde31e8732e38b5d32d1ac
Jiangcheng Bao <jbao605@gmail.com>
2021-01-21 11:29:18 -0500

    lc0956 tallest billboard - add different thinking approach, consider it as array sum problem, for given array (each element as original array element multiplied by 1, 0, -1 ), looking for largest sum of positive numbers, while keeping total as 0.

--- lc0956_tallest_billboard.py
+++ lc0956_tallest_billboard.py
@@ -32,7 +32,9 @@ Note:
 The sum of rods is at most 5000.
 
 """
+
 from typing import List
+import collections
 
 """
 first impression would be a 2D knapsack problem, then it would be dp[left][right], but left right height range is 0 ~ 5000, dp[5000][5000] would be too big
@@ -61,26 +63,55 @@ Note:
 time O(N*M) - N = len(rods), M = min(3^N, sum(rods)<=5000)
   3^N because each rode can be put in left, right, or discarded.
 """
-from functools import lru_cache
-class Solution:
+
+
+class Solution0:
     def tallestBillboard(self, rods: List[int]) -> int:
         # sort rods
         offset = 5000
-        dp = [-1] * (2*offset+1) # height of -1 means this diff is not meaningful (was not achieved)
-        dp[0+offset] = 0 # initial condition, using no rods, diff is 0 (+offset)
+        dp = [-1] * (2 * offset + 1)  # height of -1 means this diff is not meaningful (was not achieved)
+        dp[0 + offset] = 0  # initial condition, using no rods, diff is 0 (+offset)
 
         for i, l in enumerate(rods):
             # rods[i] has not been used yet, use it to update all dps
-            dp_old = dp [:] # keep a copy so that with each rod, we update dp[diff] using dp from previous round of rod
-            for diff in range(-offset, offset+1):
-                if dp_old[diff+offset] == -1: # previous state is not meaningful
+            dp_old = dp[:]  # keep a copy so that with each rod, we update dp[diff] using dp from previous round of rod
+            for diff in range(-offset, offset + 1):
+                if dp_old[diff + offset] == -1:  # previous state is not meaningful
                     continue
-                if diff+l+offset < 2*offset+1:
-                    dp[diff+l+offset] = max(dp[diff+l+offset], dp_old[diff+offset]+l)
-                if diff-l+offset >= 0:
-                    dp[diff-l+offset] = max(dp[diff-l+offset], dp_old[diff+offset])
+                if diff + l + offset < 2 * offset + 1:
+                    dp[diff + l + offset] = max(dp[diff + l + offset], dp_old[diff + offset] + l)
+                if diff - l + offset >= 0:
+                    dp[diff - l + offset] = max(dp[diff - l + offset], dp_old[diff + offset])
 
-        return dp[0+offset]
+        return dp[0 + offset]
+
+
+"""
+observation, its a knapsack problem, for each rod, we can add its height to left, or right, or not used, so the problem can be considered as:
+
+Given a list of numbers, multiple each number with 1, or 0, or -1, make the sum of all numbers to 0. Find a combination which has largest sum of all positive numbers.
+
+We can consider the sum as the key / index of dp, and the positive number sum as the value of dp, loop through all rods, for each rod, check each dp index (sum) to see if it will be updated by this rod, actually only dp[s+i], dp[s-i] might be updated by this rod, from dp[s], dp[s+i] or dp[s-i] from dp in previous round
+
+base case dp[0] = 0 # uses no rods
+
+"""
+
+class Solution:
+    def tallestBillboard(self, rods: List[int]) -> int:
+        dp = dict()
+        dp[0] = 0
+
+        for i in rods:
+            cur = collections.defaultdict(int)
+            for s in dp:
+                cur[s + i] = max(cur[s + i], dp[s] + i)
+                cur[s] = max(cur[s], dp[s])
+                cur[s - i] = max(cur[s - i], dp[s] - i)
+            dp = cur
+
+        return dp[0]
+
 
 def main():
     sol = Solution()
