8307945434e7104a5147abb2c363ee5a46e967da
Jiangcheng Bao <jbao605@gmail.com>
2021-02-02 11:01:32 -0500

    lc0547 number of provinces - disjoint set union find

---
+++ lc0547_number_of_provinces.py
@@ -0,0 +1,99 @@
+"""
+547. Number of Provinces
+Medium
+
+There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
+
+A province is a group of directly or indirectly connected cities and no other cities outside of the group.
+
+You are given an n x n matrix isConnected where isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0 otherwise.
+
+Return the total number of provinces.
+
+Example 1:
+
+Input: isConnected = [[1,1,0],[1,1,0],[0,0,1]]
+Output: 2
+Example 2:
+
+
+Input: isConnected = [[1,0,0],[0,1,0],[0,0,1]]
+Output: 3
+
+
+Constraints:
+
+1 <= n <= 200
+n == isConnected.length
+n == isConnected[i].length
+isConnected[i][j] is 1 or 0.
+isConnected[i][i] == 1
+isConnected[i][j] == isConnected[j][i]
+
+"""
+from typing import List
+
+"""
+disjoinset union find
+"""
+
+class DSU:
+    def __init__(self, n):
+        self.parent = list(range(n))
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        xp, yp = self.find(x), self.find(y)
+        if xp != yp:
+            self.parent[yp] = xp
+
+
+class DSUByRank:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [1 for _ in range(n)]
+
+    def find(self, x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self, x, y):
+        xp, yp = self.find(x), self.find(y)
+        if xp != yp:
+            if self.rank[xp] > self.rank[yp]:
+                self.parent[yp] = xp
+            elif self.rank[xp] < self.rank[yp]:
+                self.parent[xp] = yp
+            else:  # same rank, increase by 1
+                self.parent[yp] = xp
+                self.rank[xp] += 1
+
+
+class Solution:
+    def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n = len(isConnected)
+        dsu = DSUByRank(n)
+        count = n
+        for i in range(n):
+            for j in range(n):
+                if isConnected[i][j] == 1:
+                    if dsu.find(i) != dsu.find(j):
+                        dsu.union(i, j)
+                        count -= 1
+
+        return count
+
+
+def main():
+    sol = Solution()
+    assert sol.findCircleNum([[1,1,0],[1,1,0],[0,0,1]]) == 2, 'fails'
+
+    assert sol.findCircleNum([[1,0,0],[0,1,0],[0,0,1]]) == 3, 'fails'
+
+if __name__ == '__main__':
+   main()
