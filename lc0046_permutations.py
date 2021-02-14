3aaaa343541c2bb31c7734c983d5a573dc0087f6
Jiangcheng Bao <jbao605@gmail.com>
2021-02-14 17:53:34 -0500

    lc0046 permutations - backtrack (recursive) and iterative methods

--- lc0046_permutations.py
+++ lc0046_permutations.py
@@ -28,41 +28,65 @@ All the integers of nums are unique.
 from typing import List
 
 """
-dfs/backtracking
+backtracking
+in each recursive step, do this:
+1. if the partial result is of desired length, add to global result sets
+2. loop and take one number from remaining available numbers, append to partial result (path)
 
-1. iterate through the nums list, pick one element, append it to current running partial list (a path from tree root to current node), to build new partial list
-2. along with remaining available elements of nums, to pass into recursive call.
-3. If the running partial list has expected length, add it to global result list
+backtrack(nums[:i]+nums[i+1:], path+[nums[i]])
 
-https://medium.com/@CalvinChankf/a-general-approach-for-subsets-combinations-and-permutations-5c8fe3aff0ae
+"""
+class Solution0:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        n = len(nums)
+        res = []
+        def backtrack(nums, path):
+            nonlocal res
+            if len(path) == n:
+                res.append(path)
+            for i in range(len(nums)):
+                backtrack(nums[:i]+nums[i+1:], path+[nums[i]])
 
-#   available characters , chosen characters
-dfs(nums[:i] + nums[i+1:], path+ [nums[i]])
+        backtrack(nums, [])
+        return res
 
+"""
+iterative - build from n-1 to n, for length n permutation, insert n-th element into every possible insertion position of the n-1 element result from previous run
+
+Permutations of two elements are 1 2 and 2 1.
+Permutations of three elements can be obtained by inserting 3 at different positions in all permutations of size 2.
+Inserting 3 in different positions of 1 2 leads to 1 2 3, 1 3 2 and 3 1 2.
+Inserting 3 in different positions of 2 1 leads to 2 1 3, 2 3 1 and 3 2 1.
 """
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        res = []
-        def dfs(arr, path):
-            nonlocal res
-            nonlocal n
-            if len(path) == n:
-                res.append(path)
-            for i in range(len(arr)):
-                dfs(arr[:i] + arr[i+1:], path + [arr[i]])
+        result = []
+        perms = [[]]
+        for num in nums:
+            prev_perms = list(perms)
+            perms = []
+            for p in prev_perms:
+                if not p:
+                    perms = [[nums[0]]]
+                else:
+                    for j in range(len(p)+1):
+                        perms.append(p[:j] + [num] +p[j:])
 
-        res = []
-        dfs(nums, [])
-        return res
+            for np in perms:
+                if len(np) == n:
+                    result.append(np)
+
+        return result
+
 
 def main():
     sol = Solution()
-    assert sol.permute([1,2,3]) == [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]], 'fails'
+    assert sorted(sol.permute([1,2,3])) == sorted([[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]), 'fails'
 
-    assert sol.permute([0,1]) == [[0,1],[1,0]], 'fails'
+    assert sorted(sol.permute([0,1])) == sorted([[0,1],[1,0]]), 'fails'
 
-    assert sol.permute([1]) == [[1]], 'fails'
+    assert sorted(sol.permute([1])) == sorted([[1]]), 'fails'
 
 if __name__ == '__main__':
    main()
