1dd4fc5a36463397fd9cfc84ae6c2ce8f2f925a4
Jiangcheng Bao <jbao605@gmail.com>
2021-02-14 20:37:09 -0500

    lc0037 soduku solver - backtrack with row, col. base case is row==9, if col==9 => row+=1, col=0, for each digits we try on cell with '.', we check to make sure entire row, entire col and entire 3x3 square are valid.

--- lc0037_sudoku_solver.py
+++ lc0037_sudoku_solver.py
@@ -40,7 +40,7 @@ for any empty cell [i][j], try all digits (123456789), if a given digit is valid
 
 """
 
-class Solution:
+class Solution0:
     def solveSudoku(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
@@ -80,6 +80,79 @@ class Solution:
 
         dfs(board, 0, 0)
         return
+
+
+"""
+backtrack(board, row, col)
+1. base condition is if row == 9 we are done, return true
+2. for each cell that is digits (existing, or tried with a valid option), we continue to next cell row, col+1, if col+1==9, wrap around to next cell row+1, col=0
+3. for each cell that is '.', we try all digits of 123456789 in order, if with any digit, we need to verify entire row, entire column, and the 3x3 square it is in is also valid, if all valid, we proceed to next cell (row, col+1), if further processing returns False, we back out this step (restore it to '.'), and try next digits, if all digits fail, we return False to caller and return to previous step.
+
+mistakes:
+    1. board cell value string vs int
+    2. check cells with same row, or same col, or the square (row, col) is in:
+        for i in range(3*(row//3), 3*(row//3)+3):
+            for j in range(3*(col//3), 3*(col//3)+3):    
+"""
+
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
+        self.solve(board, 0, 0)
+
+    def validate(self, board, row, col, val):
+        if row < 0 or row >= 9 or col < 0 or col >= 9:
+            return False
+
+        row_vals = [board[row][j] for j in range(9) if board[row][j] != '.']
+        if str(val) in row_vals:
+            return False
+
+        col_vals = [board[i][col] for i in range(9) if board[i][col] != '.']
+        if str(val) in col_vals:
+            return False
+
+        square_vals = []
+        for i in range(3 * (row // 3), 3 * (row // 3) + 3):
+            for j in range(3 * (col // 3), 3 * (col // 3) + 3):
+                if board[i][j] != '.':
+                    square_vals.append(board[i][j])
+        if str(val) in square_vals:
+            return False
+
+        return True
+
+    def solve(self, board, row, col):
+        """
+        recursive solve board at (row, col),
+        only return True if all cells are properly set (at row==9),
+        else return False
+        """
+        if col >= 9:
+            col = col % 9
+            row += 1
+        if row == 9:
+            return True  # Done
+
+        if board[row][col] != '.':
+            if self.solve(board, row, col + 1):
+                return True
+        else:
+            for i in range(1, 10):
+                if self.validate(board, row, col, i):
+                    board[row][col] = str(i)
+                    if self.solve(board, row, col + 1):
+                        return True
+                    else:
+                        continue
+
+            # none of the digits worked, restore to . and back to previous step (return False)
+            board[row][col] = '.'
+            return False
+
 
 def main():
     sol = Solution()
