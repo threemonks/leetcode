b1e2c7a317dfe0063f0a5cae1a709097bf6f3852
Jiangcheng Bao <jbao605@gmail.com>
2021-02-16 19:57:34 -0500

    lc0241 different ways to add parenthesis - divide and conquer - catalan number

---
+++ lc0241_different_ways_to_add_parentheses.py
@@ -0,0 +1,68 @@
+"""
+241. Different Ways to Add Parentheses
+Medium
+
+Given a string of numbers and operators, return all possible results from computing all the different possible ways to group numbers and operators. The valid operators are +, - and *.
+
+Example 1:
+
+Input: "2-1-1"
+Output: [0, 2]
+Explanation:
+((2-1)-1) = 0
+(2-(1-1)) = 2
+Example 2:
+
+Input: "2*3-4*5"
+Output: [-34, -14, -10, -10, 10]
+Explanation:
+(2*(3-(4*5))) = -34
+((2*3)-(4*5)) = -14
+((2*(3-4))*5) = -10
+(2*((3-4)*5)) = -10
+(((2*3)-4)*5) = 10
+"""
+from typing import List
+
+from functools import lru_cache
+
+"""
+divide and conquer 
+recursively solve each half, and generate all possible combinations using each result from left, and each result from right, with the operator in between
+base case is pure digits (number)
+
+time complexity O(P(N)) - Catalan number P(N) = sum(p1*p_n-1+p2*p_n-2+ ... + p_n-1*p1)
+"""
+
+
+class Solution:
+    def diffWaysToCompute(self, input: str) -> List[int]:
+
+        @lru_cache(None)
+        def helper(s):
+            if s.isdigit():
+                return [int(s)]
+            result = []
+            for i, c in enumerate(s):
+                if c in '+-*':
+                    left = helper(s[:i])
+                    right = helper(s[i + 1:])
+                    if c == '+':
+                        result.extend([l + r for l in left for r in right])
+                    elif c == '-':
+                        result.extend([l - r for l in left for r in right])
+                    elif c == '*':
+                        result.extend([l * r for l in left for r in right])
+            return result
+
+        return helper(input)
+
+
+def main():
+    sol = Solution()
+    assert sorted(sol.diffWaysToCompute("2-1-1")) == sorted([2, 0]), 'fails'
+
+    assert sorted(sol.diffWaysToCompute("2*3-4*5")) == sorted([-34, -10, -14, -10, 10]), 'fails'
+
+if __name__ == '__main__':
+   main()
