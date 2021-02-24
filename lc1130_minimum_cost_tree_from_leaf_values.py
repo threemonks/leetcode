edef5e26b63cfa71272dab3988d46916b4e30194
Jiangcheng Bao <jbao605@gmail.com>
2021-02-24 16:19:44 -0500

    lc1130 minimum cost tree from leaf values - observation this is just an array problem that can be solved using Greedy method

--- lc1130_minimum_cost_tree_from_leaf_values.py
+++ lc1130_minimum_cost_tree_from_leaf_values.py
@@ -54,7 +54,7 @@ DP topdown with caching
 time O(N^3) - cache stack with two indicies, l, r, plus within the helper function, we have k loop from l to r
 space O(N^2) - cache stack with two indcies, l, r
 """
-class Solution:
+class Solution0:
     def mctFromLeafValues(self, arr: List[int]) -> int:
 
         @lru_cache(None)
@@ -70,8 +70,18 @@ class Solution:
 
         return helper(0, len(arr) - 1)
 
+
 """
-DP bottom up
+DP (brutal force)
+
+intution:
+
+We are asked to build an optimal binary tree, with all its leaves in-order traverse represented by array arr, but we don't know which leaf node belong to left subtree and which leaf node goes to right subtree.
+Since the given leaf nodes are in-order traverse, there will be a pivot point where all left side of it goes to left subtree, all right of it goes to right subtree. For each subtree, if we know the minimum sum, we can use it to build the parent tree.
+So the original problem can be broken down into subproblems via the pivot point. Then we have the following transition function, where res(i, j) repreents minimum non-leaf nodes sum with leaf nodes represented by arr[i,j]
+
+for k = i ... j:
+    res(i,j) = min(res(i,k) + res(k+1,j) + max(arr[i:k])*max(arr[k+1:j]))
 
 time O(N^3)
 space O(N^2)
@@ -92,9 +102,71 @@ class Solution1:
 
         return dp[0][n-1]
 
+"""
+Greedy
+
+https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/discuss/951938/Don't-overthink-about-trees.-It's-a-DPGreedy-problem.
+https://leetcode.com/problems/minimum-cost-tree-from-leaf-values/discuss/349098/From-O(N2)-to-O(n).-Greedy
+
+Intution
+1. Since given array is the inorder traversal of tree leaves. Their order won't change. You can only decide which 2 consecutive nodes you wanna combine to make a new node by their product.
+2. Each time we cacluate a value for a non-leaf node, we discard the smaller leave and keep the large leave, because the smaller leave will be "shadowed" by the large leave forever. To minimize the cost, we want to save the large leaves to the last, and use the small leaves first.
+3. So at each step, we pick the least product of two neighboring nodes a[i]*a[i+1], add the sum to result, and drop the smaller of the two
+4. and repeat 3
+5. until there's only one number left in the array
+
+O(N^2)
+
+"""
+
+class Solution2:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        n = len(arr)
+        res = 0
+        nums = arr[:]
+        while len(nums) > 1:
+            i = nums.index(min(nums))
+            res += min(nums[i-1:i] + nums[i+1:i+2])*nums.pop(i)
+
+        return res
+
+"""
+Monotonic decreasing stack
+
+steps:
+1. At each step, if a smaller value comes up, push into stack
+2. if a larger value comes up, we use the smallest value at top of stack (stack[-1]) to calculate a product between that number and its two neighbors (stack[-1] and nums[i]), add this product to result, then pop the smallest value at top of stack, 
+3. if after poping stack[-1], new stack top stack[-1] is still smaller than this new value nums[i], repeat above process until we can put this large value into stack.
+4. the process ends until there's only value left.
+
+Why decreasing stack - Because we use small leaf once and discard them, large leaf stays.
+
+O(N)
+"""
+import math
+class Solution:
+    def mctFromLeafValues(self, arr: List[int]) -> int:
+        n = len(arr)
+        res = 0
+        stack = [math.inf] # add sentinel value
+        for a in arr:
+            while stack[-1] <= a:
+                b = stack.pop()
+                res += b * min(a, stack[-1])
+                # print('a=%s stack=%s res=%s' % (a, stack, res))
+            stack.append(a)
+
+        # print('stack=%s' % stack)
+        while len(stack) > 2: #finish processing any numbers in stack until there's only one left # one sentinel value
+            res += stack.pop() * stack[-1]
+
+
+        return res
+
+
 
 def main():
-    sol = Solution1()
+    sol = Solution()
     assert sol.mctFromLeafValues([6,2,4]) == 32, 'fails'
 
 if __name__ == '__main__':
