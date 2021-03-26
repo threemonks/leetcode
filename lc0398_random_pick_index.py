606e55b6c1064b991727c644c7ed8d77e1e2c066
Jiangcheng Bao <jbao605@gmail.com>
2021-03-26 10:55:14 -0400

    lc0398 Reservoir Sampling - iterate all nums, increase count when nums[i]==target, replace result with new pick when randint(0, count) == 0

--- lc0398_random_pick_index.py
+++ lc0398_random_pick_index.py
@@ -22,15 +22,14 @@ solution.pick(1);
 import collections
 import random
 from typing import List
+import random
 
+import random
 """
-Reservoir Sampling
-
 for elements with duplicates, store its index as a list, when asked to pick this number, we pick one index from the list randomly
 """
 
-
-class Solution:
+class Solution0:
 
     def __init__(self, nums: List[int]):
         self.pos = collections.defaultdict(list)
@@ -38,13 +37,45 @@ class Solution:
         for i, num in enumerate(nums):
             self.pos[num].append(i)
 
+
     def pick(self, target: int) -> int:
-        return self.pos[target][random.randint(0, len(self.pos[target]) - 1)]
+        return self.pos[target][random.randint(0,len(self.pos[target])-1)]
+
 
 
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
 # param_1 = obj.pick(target)
+
+"""
+Resevoir sampling
+
+count = 0
+nums size is n
+for any index i in 0...n, we pick index i with probabliy 1/count if nums[i]==target,
+then when the process finishes, all items have been picked with probability 1/n
+
+Note: first item index=0 is always picked, but with 1/2 probability the second item (index=1) will be picked to replace index 0, and index 2 will be picked with probability 1/3 to replace previous result, so on
+
+mistakes:
+1. random.randint(0, n) is inclusiving on both end, i.e., it will pick any number from 0 to n with 1/(n+1) probability
+2. we increase count only after we found target == nums[i], i.e., we are counting how many target we have seen in nums so far, then we replace result with the new index with probability 1/count, randint(0, count)==0
+"""
+
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def pick(self, target: int) -> int:
+        res, count = -1, 0
+        for i, num in enumerate(self.nums):
+            if target == num:
+                count += 1
+                if random.randint(0, count-1) == 0: # starts from randint(0, 0), always pick index 0, 1/2 probability pick index 1, etc
+                    res = i
+
+        return res
 
 
 def main():
