562993195febd94d70f51446f513b88e833dd0c9
Jiangcheng Bao <jbao605@gmail.com>
2021-03-28 17:00:18 -0400

    lc0300 Longest Increasing Subsequence (LIS) O(Nlog(N)) solution using Binary search for tails - holding smallest tail of all increasing subsequences with length i+1

--- lc0300_longest-increasing-subsequence.py
+++ lc0300_longest-increasing-subsequence.py
@@ -53,7 +53,7 @@ time O(N^2)
 """
 
 
-class Solution:
+class Solution0:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
 
@@ -70,6 +70,51 @@ class Solution:
 
         return max(dp)  # longest increasng subseq may not end at last char
 
+
+"""
+DP with Binary Search + Monotonic decreasing array
+
+tails is an array storing the smallest tail of all increasing subsequences with length i+1 in tails[i].
+For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
+
+len = 1   :      [4], [5], [6], [3]   => tails[0] = 3
+len = 2   :      [4, 5], [5, 6]       => tails[1] = 5
+len = 3   :      [4, 5, 6]            => tails[2] = 6
+
+We can easily prove that tails is a increasing array. Therefore it is possible to do a binary search in tails array to find the one needs update.
+
+Each time we only do one of the two:
+
+(1) if x is larger than all tail element, append it at end, increase size by 1
+(2) if tails[i-1] < x <= tails[i], update tails[i]
+
+this will maintain tails invariant, and the final answer is size
+
+
+time O(Nlog(N))
+"""
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        tails = [0 for _ in range(n)]
+        size = 0
+
+        for x in nums:
+            # binary search and update tails[i]
+            i, j = 0, size
+            while i < j:
+                m = (i + j) // 2
+                if tails[m] < x:
+                    i = m + 1
+                else:
+                    j = m
+            # now i points at right boundary of arrays <x
+            tails[i] = x
+            size = max(i + 1, size)  # update size to i+1 if that is larger
+
+        return size
 
 def main():
     sol = Solution()
