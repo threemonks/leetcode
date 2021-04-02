b4b2115b824edc04380e38d5477fb19f45c8124b
Jiangcheng Bao <jbao605@gmail.com>
2021-04-02 16:08:09 -0400

    lc0033 search in rotated sorted array - Binary search to find pivot (smallest value), then binary search in either half to find the target.

---
+++ lc0033_search-in-rotated-sorted-array.py
@@ -0,0 +1,114 @@
+"""
+33. Search in Rotated Sorted Array
+Medium
+
+7307
+
+642
+
+Add to List
+
+Share
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
+
+"""
+from typing import List
+
+"""
+Binary Search
+
+1. use binary search to find the rotation index (smallest item) index pivot, which divides the array into two parts, both are sorted in itself
+2. compare target with nums[0] and nums[pivot] to see which part the target should be in
+    if nums[pivot] <= target <= nums[n-1]: target is on right side between pivot and n-1
+    else target is on left side between 0 and pivot-1
+3. in that part, use binary search to find the target
+
+Observation: since the array is rotated at one pivot once, one part of the array (to one side of the pivot index) is not rotated.
+
+
+time O(log(N))
+"""
+
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+
+        if n == 1:
+            return 0 if nums[0] == target else -1
+
+        lo, hi = 0, n - 1
+        while lo < hi:
+            mi = lo + (hi - lo) // 2
+            if nums[mi] > nums[hi]:
+                # this is not correct order, so smallest value is between mi and hi
+                lo = mi + 1
+            else:  # nums[pivot] > nums[lo] since all nums unique
+                hi = mi
+
+        # when search finishes, lo is the smallest value index
+        pivot = lo
+        # print('lo=%s hi=%s pivot=%s' % (lo, hi, pivot))
+
+        # now we found pivot, we know nums[0...pivot] and nums[pivot...n] are both sorted
+        # we search in [lo, hi) (left close, right open)
+        if nums[pivot] <= target <= nums[n - 1]:  # if target is in right part, then search in right part
+            lo, hi = pivot, n
+        else:  # target >= nums[pivot], then search in left part
+            lo, hi = 0, pivot
+
+        # print('lo=%s hi=%s' % (lo, hi))
+        while lo < hi:
+            mi = lo + (hi - lo) // 2
+            # print('lo=%s hi=%s mi=%s' % (lo, hi, mi))
+            if nums[mi] == target:
+                return mi
+            elif nums[mi] > target:
+                hi = mi
+            else:
+                lo = mi + 1
+
+        return -1
+
+
+def main():
+    sol = Solution()
+    assert sol.search(nums = [4,5,6,7,0,1,2], target = 0) == 4, 'fails'
+
+    assert sol.search(nums = [4,5,6,7,0,1,2], target = 3) == -1, 'fails'
+
+    assert sol.search(nums = [1], target = 0) == -1, 'fails'
+
+    assert sol.search([5,1,3], 1) == 1, 'fails'
+
+
+if __name__ == '__main__':
+   main()
