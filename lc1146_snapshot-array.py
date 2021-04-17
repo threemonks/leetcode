6e4f3a4bf4ee36cf7de2e96f2b633920fefa6ceb
Jiangcheng Bao <jbao605@gmail.com>
2021-04-16 21:40:26 -0400

    lc1146 snapshot array - Design Array using Hash Table

--- lc1146_snapshot-array.py
+++ lc1146_snapshot-array.py
@@ -70,13 +70,6 @@ class SnapshotArray:
 # param_2 = obj.snap()
 # param_3 = obj.get(index,snap_id)
 
-"""
-SnapshotArray snapshotArr = new SnapshotArray(3); // set the length to be 3
-snapshotArr.set(0,5);  // Set array[0] = 5
-snapshotArr.snap();  // Take a snapshot, return snap_id = 0
-snapshotArr.set(0,6);
-snapshotArr.get(0,0);  // Get the value of array[0] with snap_id = 0, return 5
-"""
 def main():
         obj = SnapshotArray(3)
         obj.set(0, 5)
