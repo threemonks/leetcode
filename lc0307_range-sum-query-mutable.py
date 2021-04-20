80e0bb301bfd2fb78bdcf104fc4ca14f60dadd02
Jiangcheng Bao <jbao605@gmail.com>
2021-04-20 12:51:25 -0400

    lc0307 range sum query mutable - Segment Tree

--- lc0307_range-sum-query-mutable.py
+++ lc0307_range-sum-query-mutable.py
@@ -1,6 +1,46 @@
 """
 307. Range Sum Query - Mutable
 Medium
+
+1781
+
+106
+
+Add to List
+
+Share
+Given an array nums and two types of queries where you should update the value of an index in the array, and retrieve the sum of a range in the array.
+
+Implement the NumArray class:
+
+NumArray(int[] nums) initializes the object with the integer array nums.
+void update(int index, int val) updates the value of nums[index] to be val.
+int sumRange(int left, int right) returns the sum of the subarray nums[left, right] (i.e., nums[left] + nums[left + 1], ..., nums[right]).
+
+
+Example 1:
+
+Input
+["NumArray", "sumRange", "update", "sumRange"]
+[[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
+Output
+[null, 9, null, 8]
+
+Explanation
+NumArray numArray = new NumArray([1, 3, 5]);
+numArray.sumRange(0, 2); // return 9 = sum([1,3,5])
+numArray.update(1, 2);   // nums = [1,2,5]
+numArray.sumRange(0, 2); // return 8 = sum([1,2,5])
+
+
+Constraints:
+
+1 <= nums.length <= 3 * 10^4
+-100 <= nums[i] <= 100
+0 <= index < nums.length
+-100 <= val <= 100
+0 <= left <= right < nums.length
+At most 3 * 10^4 calls will be made to update and sumRange.
 """
 from typing import List
 
