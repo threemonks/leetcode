6f86b7ca146837f3d58bc6f01d71c23ecd9b4eb2
Jiangcheng Bao <jbao605@gmail.com>
2021-04-30 13:00:42 -0400

    lc0113 path sum ii - tree traverse

---
+++ lc0113_path-sum-ii.py
@@ -0,0 +1,70 @@
+"""
+113. Path Sum II
+Medium
+
+2732
+
+85
+
+Add to List
+
+Share
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
+
+A leaf is a node with no children.
+
+
+
+Example 1:
+
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Example 2:
+
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 5000].
+-1000 <= Node.val <= 1000
+-1000 <= targetSum <= 1000
+"""
+
+
+# Definition for a binary tree node.
+from typing import List
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def pathSum(self, root: TreeNode, targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+
+        ans = []
+
+        def dfs(node, path, sums):
+            if not node.left and not node.right:
+                if sums == targetSum:
+                    ans.append(path)
+                return
+            if node.left:
+                dfs(node.left, path + [node.left.val], sums + node.left.val)
+            if node.right:
+                dfs(node.right, path + [node.right.val], sums + node.right.val)
+
+        dfs(root, [root.val], root.val)
+
+        return ans
