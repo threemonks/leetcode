066f9f6958cd6377de6441f35c356176dc64164f
Jiangcheng Bao <jbao605@gmail.com>
2021-05-02 12:26:33 -0400

    lc0031 next permutation - add comment on steps

--- lc0031_next-permutation.py
+++ lc0031_next-permutation.py
@@ -51,9 +51,16 @@ Next Permutation (lexcigological order)
 3. Swap nums[k] and nums[l]
 4. Reverse the sub-array nums[k + 1:]
 
+步骤
+1. 从右边开始找到第一个不符合递增的数字nums[i]，from right end, first nums[i]<nums[i+1]
+   如果找不到，则整个数组是降序，没有next permutation，直接整个数组反序
+2. 从右边开始找到大于nums[i]但是尽可能小的数字nums[j]，就是从右边第一个大于nums[i]的数字，因为该段子数组是从右往左递增
+3. 交换这两个数字，swap nums[i] and nums[j]
+4. 把i+1到数组右端后缀子数组反序, reverse nums[i+1:]
+
 how to understand it:
-step-1: easy, find the first digit (pivot) that can be swapped to make permutation bigger (pivot element before longest non-increasing suffix)
-step-2: easy, find the digit bigger but closest to nums[k] (right most successor to pivot)
+step-1: find the first digit (pivot) that can be swapped to make permutation bigger (pivot element before longest non-increasing suffix)
+step-2: find the digit bigger but closest to nums[k] (right most successor)
 step-3: swap(nums[k], nums[l])
 step-4: sort the subarray nums[k+1:end], why we can just reverse instead of sort?
         because we know nums[k+1:end] must be non-increasing, reason:
@@ -64,12 +71,11 @@ step-4: sort the subarray nums[k+1:end], why we can just reverse instead of sort
 example:
 2,3,6,5,4,1 
 1. find last number nums[i]=3 that can be swapped to make larger (first non-ascending from right most to left)
-   if cannot find, the entire array is ascending, no next, just reverse the whole thing
-2. find first number nums[j] from right that is larger then nums[i]=3 but as small as possible: 4
+   if cannot find, the entire array is descending, no next, just reverse the whole thing
+2. find first number nums[j] from right that is larger then nums[i]=3 but as small as possible: 4, first one that's larger than 4 from right, since that part is increasing while going to left
 3. swap nums[i]=3 and nums[j]=4
 4. reverse nums[i+1:] (need to sort it asecnding, but since it was ascending from right, or descending, but after swap, it is still descending)
 """
-
 
 class Solution:
     def nextPermutation(self, nums: List[int]) -> None:
@@ -77,37 +83,46 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         n = len(nums)
-
-        def reverse(l, r):
-            # reverse nums[l...r]
-            nonlocal nums
-            while (l < r):
-                nums[l], nums[r] = nums[r], nums[l]
-                l += 1
-                r -= 1
-
-        # find last number that is smaller than it's following number
-        idx1 = -1
-        for i in range(n - 2, -1, -1):
-            if nums[i] < nums[i + 1]:
-                idx1 = i
-                break
-        # print('idx1=%s' % idx1)
-        if idx1 == -1:
-            reverse(0, n - 1)
+        if n == 1:
             return
 
-        # find smallest number to right of idx1 that is larger than nums[idx1]
-        idx2 = -1
-        for j in range(n - 1, idx1, -1):
-            if nums[j] > nums[idx1] and (idx2 == -1 or nums[j] < nums[idx2]):
-                idx2 = j
+        def swap(i, j):
+            nonlocal nums
+            tmp = nums[i]
+            nums[i] = nums[j]
+            nums[j] = tmp
 
-        # swap idx1 and idx2
-        nums[idx1], nums[idx2] = nums[idx2], nums[idx1]
+        def reverse(l, r):
+            # reverse subarray between l and r
+            nonlocal nums
+            i, j = l, r
+            while i < j:
+                swap(i, j)
+                i += 1
+                j -= 1
 
-        # reverse nums[idx1+1:]
-        reverse(idx1 + 1, n - 1)
+        # 1 . find first decreasing nums[i] from right such that nums[i] < nums[i+1]
+        i = n-2
+        while i>=0 and nums[i+1] <= nums[i]:
+            i -= 1
+
+        # print('i=%s' % i)
+        # if i is -1, the entire array is ascending, no next, just reverse entire array
+        if i < 0 and nums[0] > nums[1]:
+            reverse(0, n-1)
+            return
+
+        if i >= 0:
+            # 2. from right, find first nums[j] that is larger than nums[i], but as small as possible, i.e., first number that's larger than nums[i] from right
+            j = n-1
+            while j>=0 and nums[j] <= nums[i]:
+                j -= 1
+
+            # 3. swap nums[i] and nums[k]
+            swap(i, j)
+
+        # 4. reverse nums[i+1:]
+        reverse(i+1, n-1)
 
 
 def main():
