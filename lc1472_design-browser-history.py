34494088c94d6ddbb1e1332906b7ab87f2d323ee
Jiangcheng Bao <jbao605@gmail.com>
2021-07-18 11:54:28 -0400

    lc1472 design browser history - Stack

--- lc1472_design-browser-history.py
+++ lc1472_design-browser-history.py
@@ -91,15 +91,7 @@ class BrowserHistory:
 
 
 def main():
-    """
-    Input:
-["BrowserHistory","visit","visit","visit","back","back","forward","visit","forward","back","back"]
-[["leetcode.com"],["google.com"],["facebook.com"],["youtube.com"],[1],[1],[1],["linkedin.com"],[2],[2],[7]]
-Output:
-[null,null,null,null,"facebook.com","google.com","facebook.com",null,"linkedin.com","google.com","leetcode.com"]
 
-    :return:
-    """
     obj = BrowserHistory("leetcode.com")
     obj.visit("google.com")
     obj.visit("facebook.com")
