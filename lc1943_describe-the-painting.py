c9dad56965c9306450338f8f0e4b082636e281fa
Jiangcheng Bao <jbao605@gmail.com>
2021-07-24 17:19:17 -0400

    lc1943 - describe the painting - Line Sweep

---
+++ lc1943_describe-the-painting.py
@@ -0,0 +1,115 @@
+"""
+1943. Describe the Painting
+Medium
+
+36
+
+1
+
+Add to List
+
+Share
+There is a long and thin painting that can be represented by a number line. The painting was painted with multiple overlapping segments where each segment was painted with a unique color. You are given a 2D integer array segments, where segments[i] = [starti, endi, colori] represents the half-closed segment [starti, endi) with colori as the color.
+
+The colors in the overlapping segments of the painting were mixed when it was painted. When two or more colors mix, they form a new color that can be represented as a set of mixed colors.
+
+For example, if colors 2, 4, and 6 are mixed, then the resulting mixed color is {2,4,6}.
+For the sake of simplicity, you should only output the sum of the elements in the set rather than the full set.
+
+You want to describe the painting with the minimum number of non-overlapping half-closed segments of these mixed colors. These segments can be represented by the 2D array painting where painting[j] = [leftj, rightj, mixj] describes a half-closed segment [leftj, rightj) with the mixed color sum of mixj.
+
+For example, the painting created with segments = [[1,4,5],[1,7,7]] can be described by painting = [[1,4,12],[4,7,7]] because:
+[1,4) is colored {5,7} (with a sum of 12) from both the first and second segments.
+[4,7) is colored {7} from only the second segment.
+Return the 2D array painting describing the finished painting (excluding any parts that are not painted). You may return the segments in any order.
+
+A half-closed segment [a, b) is the section of the number line between points a and b including point a and not including point b.
+
+
+
+Example 1:
+
+
+Input: segments = [[1,4,5],[4,7,7],[1,7,9]]
+Output: [[1,4,14],[4,7,16]]
+Explanation: The painting can be described as follows:
+- [1,4) is colored {5,9} (with a sum of 14) from the first and third segments.
+- [4,7) is colored {7,9} (with a sum of 16) from the second and third segments.
+Example 2:
+
+
+Input: segments = [[1,7,9],[6,8,15],[8,10,7]]
+Output: [[1,6,9],[6,7,24],[7,8,15],[8,10,7]]
+Explanation: The painting can be described as follows:
+- [1,6) is colored 9 from the first segment.
+- [6,7) is colored {9,15} (with a sum of 24) from the first and second segments.
+- [7,8) is colored 15 from the second segment.
+- [8,10) is colored 7 from the third segment.
+Example 3:
+
+
+Input: segments = [[1,4,5],[1,4,7],[4,7,1],[4,7,11]]
+Output: [[1,4,12],[4,7,12]]
+Explanation: The painting can be described as follows:
+- [1,4) is colored {5,7} (with a sum of 12) from the first and second segments.
+- [4,7) is colored {1,11} (with a sum of 12) from the third and fourth segments.
+Note that returning a single segment [1,7) is incorrect because the mixed color sets are different.
+
+
+Constraints:
+
+1 <= segments.length <= 2 * 10^4
+segments[i].length == 3
+1 <= starti < endi <= 10^5
+1 <= colori <= 10^9
+Each colori is distinct.
+
+"""
+from collections import defaultdict
+from typing import List
+
+"""
+Line Sweep
+
+we keep adding color if its start of segment, or substracting color for segment end, and for same sum in different segments, we just need to add a new section into result whenever there's a segment start or end.
+
+mistakes:
+1. don't need to keep track of different color mix resulting in same sum, just need to always output a new segment for every start or end event, if color > 0
+2. don't add segement to output if color mix == 0
+3. color mix always carry forward, since events contains color change/delta at each event
+
+time O(Nlog(N))
+space O(N)
+"""
+
+
+class Solution:
+    def splitPainting(self, segments: List[List[int]]) -> List[List[int]]:
+
+        events = defaultdict(int)
+        for s, e, c in segments:  # how about {1,3} at time 1, {2, 3} at time 2, and {1,2,3} again at time 3?
+            events[s] += c
+            events[e] -= c
+
+        print(events)
+        ans = []
+        start, color = None, 0
+        for t, c in sorted(events.items()):
+            if start is not None and color > 0:
+                ans.append([start, t, color])
+            start = t
+            color += c
+
+        return ans
+
+
+def main():
+    sol = Solution()
+    assert sol.splitPainting(segments = [[1,4,5],[4,7,7],[1,7,9]]) == [[1,4,14],[4,7,16]], 'fails'
+
+    assert sol.splitPainting(segments = [[1,7,9],[6,8,15],[8,10,7]]) == [[1,6,9],[6,7,24],[7,8,15],[8,10,7]], 'fails'
+
+    assert sol.splitPainting(segments = [[1,4,5],[1,4,7],[4,7,1],[4,7,11]]) == [[1,4,12],[4,7,12]], 'fails'
+
+if __name__ == '__main__':
+   main()
