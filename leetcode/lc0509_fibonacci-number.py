fa8e4760c81db87c319296e94eb2e3d612f3ca9e
Jiangcheng Bao <jbao605@gmail.com>
2026-01-07 21:00:44 -0500

    lc 509 fibonacci number

---
+++ leetcode/lc0509_fibonacci-number.py
@@ -0,0 +1,103 @@
+"""
+509. Fibonacci Number
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+The Fibonacci numbers, commonly denoted F(n) form a sequence, called the Fibonacci sequence, such that each number is the sum of the two preceding ones, starting from 0 and 1. That is,
+
+F(0) = 0, F(1) = 1
+F(n) = F(n - 1) + F(n - 2), for n > 1.
+Given n, calculate F(n).
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: F(2) = F(1) + F(0) = 1 + 0 = 1.
+Example 2:
+
+Input: n = 3
+Output: 2
+Explanation: F(3) = F(2) + F(1) = 1 + 1 = 2.
+Example 3:
+
+Input: n = 4
+Output: 3
+Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
+ 
+
+Constraints:
+
+0 <= n <= 30
+
+"""
+
+
+"""
+DP
+
+"""
+"""
+Dynamic Programming
+
+"""
+class Solution:
+    """
+    recursive
+    """
+    def fib0(self, n: int) -> int:
+        if n <= 1:
+            return n
+        return self.fib(n-1) + self.fib(n-2)
+
+    """
+    top down
+    
+    complexity
+    time O(log(N))
+    space O(N)
+    """
+    def fib1(self, n: int) -> int:
+        if n <= 1:
+            return n
+        dp = [0 for _ in range(n+1)]
+        dp[0] = 0
+        dp[1] = 1
+        for i in range(2, n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+
+        return dp[n]
+
+    """
+    top down with space optimization
+
+    complexity
+    time O(log(N))
+    space O(1)
+    """
+    def fib(self, n: int) -> int:
+        if n <= 1:
+            return n
+        dp0 = 0
+        dp1 = 1
+        dp2 = 1
+        for i in range(2, n+1):
+            dp2 = dp1 + dp0
+            dp0, dp1 = dp1, dp2
+
+        return dp2
+
+
+
+def main():
+    sol = Solution()
+    assert sol.fib(2) == 1, 'fails'
+    assert sol.fib(3) == 2, 'fails'
+    assert sol.fib(4) == 3, 'fails'
+
+if __name__ == '__main__':
+   main()
